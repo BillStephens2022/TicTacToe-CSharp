6b2bf97c775518ebb1b517ee88ebcc48233e591c
BillStephens2022 <two4onebill@yahoo.com>
2024-02-28 15:31:20 -0500

    created functionality to get user input by player and determine X or O based on player number, also update board values array as players input values

--- TicTacToe-CSharp/Program.cs
+++ TicTacToe-CSharp/Program.cs
@@ -12,16 +12,41 @@
 
         static void Main(string[] args)
         {
+            int playerNumber = 2; // player 1 starts
+            int userInput = 0;
+            bool isValidInput = true;
+
            SetBoard();
            Console.ReadKey();
+
+            do
+            {
+                if (playerNumber == 2)
+                {
+                    playerNumber = 1;
+                    EnterXorO(playerNumber, userInput);
+                }
+                else if (playerNumber == 1)
+                {
+                    playerNumber = 2;
+                    EnterXorO(playerNumber, userInput);
+                }
+
+                do
+                {
+                    Console.Write("\nPlayer {0}: Choose your field by number: ", playerNumber);
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                } while (!isValidInput);
+
+            } while (true);
         }
 
-        // display the TicTacToe Board
+        // display the TicTacToe Board based on the boardValues
 
         public static void SetBoard()
         {
             Console.WriteLine("     |     |     ");
-            // TODO: REPLACE NUMBERS WITH VARIABLES
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardValues[0,0], boardValues[0,1], boardValues[0, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
@@ -30,6 +55,29 @@
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardValues[2, 0], boardValues[2, 1], boardValues[2, 2]);
             Console.WriteLine("     |     |     ");
+        }
+
+        public static void EnterXorO(int playerNumber, int userInput)
+        {
+            char playerChar = ' ';
+            if (playerNumber == 1) 
+            {
+                playerChar = 'X';
+            } else if (playerNumber == 2)
+            {
+                playerChar = 'O';
+            }
+           
+            switch(userInput) 
+            {
+                case 1: boardValues[0, 0] = playerChar; break; // note using "char" type for populating the board                      
+                case 2: boardValues[0, 1] = playerChar; break;
+                case 3: boardValues[0, 2] = playerChar; break;
+                case 4: boardValues[1, 0] = playerChar; break;
+                case 5: boardValues[1, 1] = playerChar; break;
+                case 6: boardValues[1, 2] = playerChar; break;
+                case 7: boardValues[2, 0] = playerChar; break;
+            }
         }
     }
 }
