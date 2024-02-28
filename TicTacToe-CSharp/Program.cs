a60db0a69d728834f08c25cd334c4733836a3592
BillStephens2022 <two4onebill@yahoo.com>
2024-02-28 14:52:50 -0500

    added a method to display TicTacToe board

--- TicTacToe-CSharp/Program.cs
+++ TicTacToe-CSharp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           SetField();
+           Console.ReadKey();
+        }
+
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            // TODO: REPLACE NUMBERS WITH VARIABLES
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 4, 5, 6);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 7, 8, 9);
+            Console.WriteLine("     |     |     ");
         }
     }
 }
