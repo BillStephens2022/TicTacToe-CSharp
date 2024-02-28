48326a8cc4d2dced6a135319ecb36a83fc6121f2
BillStephens2022 <two4onebill@yahoo.com>
2024-02-28 15:02:04 -0500

    updated method name to SetBoard and updated to show values saved to the new boardValues array

--- TicTacToe-CSharp/Program.cs
+++ TicTacToe-CSharp/Program.cs
@@ -2,23 +2,33 @@
 {
     internal class Program
     {
+        // the playField matrix - represents the 9 boxes on a TicTacToe board
+        static char[,] boardValues =
+        {
+            {'1', '2', '3' },
+            {'4', '5', '6' },
+            {'7', '8', '9' },
+        };
+
         static void Main(string[] args)
         {
-           SetField();
+           SetBoard();
            Console.ReadKey();
         }
 
-        public static void SetField()
+        // display the TicTacToe Board
+
+        public static void SetBoard()
         {
             Console.WriteLine("     |     |     ");
             // TODO: REPLACE NUMBERS WITH VARIABLES
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardValues[0,0], boardValues[0,1], boardValues[0, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 4, 5, 6);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardValues[1, 0], boardValues[1, 1], boardValues[1, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 7, 8, 9);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardValues[2, 0], boardValues[2, 1], boardValues[2, 2]);
             Console.WriteLine("     |     |     ");
         }
     }
